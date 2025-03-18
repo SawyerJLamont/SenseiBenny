b9841624a9825653b5b0a60277c821035d875153
SawyerJLamont <145060905+SawyerJLamont@users.noreply.github.com>
2025-03-18 11:03:23 -0700

    1.1.4 Finally running well, changed first time dialouge

--- components/first-time-modal.tsx
+++ components/first-time-modal.tsx
@@ -38,9 +38,14 @@ export default function FirstTimeModal() {
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent className="sm:max-w-md">
         <DialogHeader>
-          <DialogTitle>Welcome to Japanese Conjugation Practice!</DialogTitle>
+          <DialogTitle>Welcome to Sensei Benny</DialogTitle>
           <DialogDescription>
-            This tool helps you practice Japanese verb conjugations with customizable settings.
+            If you are a Oregon State Student, this is a tool to help you practive the conjugations
+            of Japanese verbs we have learned in class. I have also added a few extra words that I think are
+            useful to know. If you are not from Oregon State, you can still use this tool to practice
+            Japanese verb conjugations. Please look at the quick guide before jumping in so that you can 
+            get the most out of this tool. If you find any errors in conjugation or experince any bugs, 
+            please let me know by emailing me at <a href="mailto: lamontsa@oregonstate.edu "></a>
           </DialogDescription>
         </DialogHeader>
         <div className="space-y-4 py-4">
